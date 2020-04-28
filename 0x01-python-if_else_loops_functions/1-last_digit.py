2a69653e6eb526d60fd48733e7cd419b1a1937c4
derric-d <derric.donehoo@gmail.com>
2020-04-28 20:17:22 +0000

    add style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-	last = number % 10 - 10
+    last = number % 10 - 10
 elif number >= 0:
-	last = number % 10
+    last = number % 10
 if last == 0:
     print("Last digit of {:d} is 0 and is 0".format(number))
 if last < 6:
