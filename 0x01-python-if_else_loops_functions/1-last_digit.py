ecdc3a7e39c85b9d1f004b3144be27d84a0d37c6
derric-d <derric.donehoo@gmail.com>
2020-04-28 20:23:18 +0000

    style and tighten ifs

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number < 0:
-    last = number % 10 - 10
-elif number >= 0:
-    last = number % 10
+last = number % 10 if number > 10 else number % -10
+
 if last == 0:
     print("Last digit of {:d} is 0 and is 0".format(number))
-if last < 6:
+if last < 6 and last is not 0:
     print("Last digit of {:d} is {:d} and is less than 6 and not 0"
           .format(number, last))
 elif last > 5:
