334028a17e2653541ed8ffb4fc5ebeb6970cdb76
derric-d <derric.donehoo@gmail.com>
2020-04-30 20:27:39 +0000

    formatting

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    print("Arguments: {}".format(len(sys.argv) - 1))
-    for i in range(len(sys.argv) - 1):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    from sys import argv as av
+    if len(av) == 1:
+        print("0 arguments.")
+    elif len(av) == 2:
+        print("{} argument:".format(len(av) - 1))
+    else:
+        print("{} arguments".format(len(av) - 1))
+    for i in range(len(av) - 1):
+        print("{}: {}".format(i + 1, av[i + 1]))
