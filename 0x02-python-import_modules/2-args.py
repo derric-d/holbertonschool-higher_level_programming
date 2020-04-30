b67f598e9fac89138d25d336876a4768424e2a21
derric-d <derric.donehoo@gmail.com>
2020-04-30 20:29:41 +0000

    formatting

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     elif len(av) == 2:
         print("{} argument:".format(len(av) - 1))
     else:
-        print("{} arguments".format(len(av) - 1))
+        print("{} arguments:".format(len(av) - 1))
     for i in range(len(av) - 1):
         print("{}: {}".format(i + 1, av[i + 1]))
