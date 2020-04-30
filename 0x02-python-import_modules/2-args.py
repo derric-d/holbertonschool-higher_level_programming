8b86640e9aae32c8ff82cd00bb0a0af7249e1062
derric-d <derric.donehoo@gmail.com>
2020-04-30 20:15:18 +0000

    add mandatories

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    print("Arguments: {}".format(len(sys.argv) - 1))
+    for i in range(len(sys.argv) - 1):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
