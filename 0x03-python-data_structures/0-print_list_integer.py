ec6f3c37b4660adf91e9415461cfe5689368d53e
derric-d <derric.donehoo@gmail.com>
2020-05-05 20:54:47 +0000

    add fixes

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for idx, j in enumerate(my_list):
-        print(format(str(j)))
+        print("{:d}".format(j))
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    l = sorted(my_list)
-    return l[-1]
+    if my_list:
+        l = sorted(my_list)
+        return l[-1]
+    else:
+        return None
