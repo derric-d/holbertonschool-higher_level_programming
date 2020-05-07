da8b9d045d171eafffe11f59d6f81a1b69780cec
derric-d <derric.donehoo@gmail.com>
2020-05-07 20:47:21 +0000

    use new copy

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda x: list(map(lambda y: y**2, x)), matrix))
+    new = matrix.copy()
+    return list(map(lambda x: list(map(lambda y: y**2, x)), new))
