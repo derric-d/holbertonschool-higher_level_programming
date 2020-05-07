0a559eac68a1ee231a8b22d2c2c640629fe6be6f
derric-d <derric.donehoo@gmail.com>
2020-05-07 21:00:49 +0000

    add diff solution

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new = map(list, matrix)
-    return ([[ele ** 2 for ele in lst] for lst in new])
+    new = [[ele ** 2 for ele in lst] for lst in matrix]
+    return new
