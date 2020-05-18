86e6f415cd0af3c1c488caa3980bce01cd6308f4
derric-d <derric.donehoo@gmail.com>
2020-05-18 20:58:55 +0000

    4 keep track of values

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,8 +2,8 @@
 def list_division(l1, l2, llen):
     reslist = []
     ele = 0
-    div = 0
     while ele < llen:
+        div = 0
         try:
             div = l1[ele] / l2[ele]
         except TypeError:
