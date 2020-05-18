eaeda79a56da2e832dde7d8f0dd271515b24de64
derric-d <derric.donehoo@gmail.com>
2020-05-18 20:36:48 +0000

    clean up logic, pass on finally

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,16 +6,16 @@ def list_division(l1, l2, list_length):
     if list_length > 0:
         for i in range(list_length):
             try:
-                div = l1[i] / l2[i]
+                ret.append(l1[i] / l2[i])
             except TypeError:
-                div = 0
+                ret.append(0)
                 print("wrong type")
             except ZeroDivisionError:
-                div = 0
+                ret.append(0)
                 print("disvision by zero")
             except IndexError:
-                div = 0
+                ret.append(0)
                 print("out of range")
             finally:
-                ret.append(div)
+                pass
     return ret
