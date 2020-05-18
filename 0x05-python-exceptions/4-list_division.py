875bf9a18d3221b9674780b4d3a53ead95b3ff0f
derric-d <derric.donehoo@gmail.com>
2020-05-18 20:32:23 +0000

    add negative check

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,18 +3,19 @@
 
 def list_division(l1, l2, list_length):
     ret = []
-    for i in range(list_length):
-        try:
-            div = l1[i] / l2[i]
-        except TypeError:
-            div = 0
-            print("wrong type")
-        except ZeroDivisionError:
-            div = 0
-            print("disvision by zero")
-        except IndexError:
-            div = 0
-            print("out of range")
-        finally:
-            ret.append(div)
+    if list_length > 0:
+        for i in range(list_length):
+            try:
+                div = l1[i] / l2[i]
+            except TypeError:
+                div = 0
+                print("wrong type")
+            except ZeroDivisionError:
+                div = 0
+                print("disvision by zero")
+            except IndexError:
+                div = 0
+                print("out of range")
+            finally:
+                ret.append(div)
     return ret
