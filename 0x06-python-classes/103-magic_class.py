449e8803463a76beee48c91f6388c0b1f48f2693
derric-d <derric.donehoo@gmail.com>
2020-05-20 15:15:38 +0000

    add docs

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import math
+"""docstr"""
 
 
 class MagicClass:
+    """docstr"""
     def __init__(self, radius=0):
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
