22e89b4f24fa9d8dfc3c6d47fdc60f24b85d13e6
derric-d <derric.donehoo@gmail.com>
2020-06-03 05:44:10 +0000

    fix error message format

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,6 +12,6 @@ class BaseGeometry(object):
     def integer_validator(self, name, value):
         """check ints"""
         if type(value) is not int:
-            raise TypeError("<name> must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
