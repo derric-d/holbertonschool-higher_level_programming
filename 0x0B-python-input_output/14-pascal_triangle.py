247685f920dd0b2b3c6d659a3608b8666f5f9a80
derric-d <derric.donehoo@gmail.com>
2020-06-04 03:28:52 +0000

    block comments need a space

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -8,19 +8,19 @@ def pascal_triangle(n):
     lst = []
     if n <= 0:
         return lst
-    #n determines rows
+    # n determines rows
     for ele in range(n):
         tmp = [1]
         if lst:
-            #now iterate for each element in each row (columns)
-            #print("loop")
+            # now iterate for each element in each row (columns)
+            # print("loop")
             for lel in range(ele):
-                #take sum of return list last element so far
-                #and the row
-                #print("in func")
-                #print(lst[-1][lel:lel+2])
+                # take sum of return list last element so far
+                # and the row
+                # print("in func")
+                # print(lst[-1][lel:lel+2])
                 tmp.append(sum(lst[-1][lel:lel+2]))
-                #print("tmp:")
-                #print(tmp)
+                # print("tmp:")
+                # print(tmp)
         lst.append(tmp)
     return lst
