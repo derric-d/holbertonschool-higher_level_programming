03bb41cef86360d6d9b0372a24fef2fdd0c72f15
derric-d <derric.donehoo@gmail.com>
2020-07-14 21:57:04 +0000

    fix of 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- list privileges of users
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- user_0d_1 exists for this project
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
