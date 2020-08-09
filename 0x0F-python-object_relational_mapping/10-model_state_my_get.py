143cdd65350c13f570d59644b5816da25e31f719
derric-d <derric.donehoo@gmail.com>
2020-08-09 18:04:22 +0000

    new session instance techn

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -3,17 +3,19 @@
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(eng)
-    sess = Session(eng)
-    state = sess.query(State).filter(State.name == argv[4]).first()
+    Session = sessionmaker()
+    Session.configure(bind=eng)
+    sess = Session()
+    state = sess.query(State).filter(State.name == argv[4]).order_by(State.id).first()
     if state:
-        print(state.id)
+        print("{}".format(state.id))
     else:
         print("Not Found")
     sess.close()
