730eef1b9c9ad117b4195c8baca63ce3f1f1a56f
derric-d <derric.donehoo@gmail.com>
2020-08-09 19:11:22 +0000

    add 102

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""use sqlalchemy to list states"""
+from sys import argv
+from relationship_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from relationship_city import City
+
+if __name__ == "__main__":
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(eng)
+    sess = Session(eng)
+    states = sess.query(State).order_by(State.id).all()
+    for state in states:
+        for city in state.cities:
+            print("{}: {} -> {}".format(city.id, city.name, state.name))
+
+    sess.commit()
+    sess.close()
