1b1a1c232d42a40b36d653c3b80b34fe974bed89
derric-d <derric.donehoo@gmail.com>
2020-08-09 18:26:50 +0000

    add error check for arg length

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -7,6 +7,8 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
+    if len(argv) != 5:
+        exit('No.')
     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(eng)
