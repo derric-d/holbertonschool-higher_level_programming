c7ef694c8e270b1d11338c18ca2154caeb276c4b
derric-d <derric.donehoo@gmail.com>
2020-08-24 20:48:41 +0000

    change import less specific

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """script to fetch url header"""
 import requests
-from sys import argv
+import sys
 
 
 if __name__ == '__main__':
     url = "http://0.0.0.0:5000/search_user"
-    load = {"q": argv[1][0] if len(argv) > 1 else ""}
+    load = {"q": sys.argv[1][0] if len(sys.argv) > 1 else ""}
     r = requests.post(url, data=load)
     try:
         if not r.json():
