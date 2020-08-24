b641e1ce51172989bde7f3a1b9c633e7d8fe2d9a
derric-d <derric.donehoo@gmail.com>
2020-08-24 20:39:57 +0000

    end of line space

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,6 @@ import requests
 
 if __name__ == '__main__':
     r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:\
-           \n\t- type: {}\n\t- content: {}"
+    print("Body response:"
+          "\n\t- type: {}\n\t- content: {}"
           .format(type(r.text), r.text))
