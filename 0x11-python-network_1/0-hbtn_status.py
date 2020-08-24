c6af8375239ff1fa5ace23fbd194270baceed14c
derric-d <derric.donehoo@gmail.com>
2020-08-24 20:51:11 +0000

    a letter

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         res = r.read()
         t = type(res)
         d = res.decode('utf-8')
-        print("Body Response:"
+        print("Body response:"
               "\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
               .format(t, res, d))
