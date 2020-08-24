39af4acee35d0c25255dc087e267385e39023a5a
derric-d <derric.donehoo@gmail.com>
2020-08-24 20:26:54 +0000

    add fixed 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """script to fetch url header"""
 from urllib import request, parse
-from sys import argv as av
+from sys import argv
 
 
 if __name__ == '__main__':
-    load = parse.urlencode({"email": av[2]}).encode()
-    with request.urlopen(av[1], load) as r:
-        print(r.read().decode('utf-8'))
+    load = parse.urlencode({"email": argv[2]}).encode('ascii')
+    with request.urlopen(argv[1], load) as r:
+        res = r.read()
+        print(res.decode('utf-8'))
